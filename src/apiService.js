c8e8694bdee606054fccb17d5bfea22e511b189f
Ostap Pluzhnik <plostvik@gmail.com>
2020-11-03 20:35:46 +0200

    Закончил работу над задачей

--- src/apiService.js
+++ src/apiService.js
@@ -1,47 +1,43 @@
-// key: 18943342-b8026c260dd173166ae4011d3
-// https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=что_искать&page=номер_страницы&per_page=12&key=твой_ключ
-
-const { data } = require("autoprefixer");
-
-// {
-//     "comments": 78,
-//     "downloads": 63296,
-//     "favorites": 558,
-//     "id": 1508613,
-//     "imageHeight": 2135,
-//     "imageSize": 1630104,
-//     "imageWidth": 2894,
-//     "largeImageURL": "https://pixabay.com/get/57e5d54b4c53af14f6da8c7dda793376173cd8e7524c704c702873dc9f44c551_1280.jpg",
-//     "likes": 575,
-//     "pageURL": "https://pixabay.com/photos/cat-animal-cat-portrait-cat-s-eyes-1508613/",
-//     "previewHeight": 110,
-//     "previewURL": "https://cdn.pixabay.com/photo/2016/07/10/21/47/cat-1508613_150.jpg",
-//     "previewWidth": 150,
-//     "tags": "cat, animal, cat portrait",
-//     "type": "photo",
-//     "user": "cocoparisienne",
-//     "userImageURL": "https://cdn.pixabay.com/user/2018/11/26/11-06-29-714_250x250.jpg",
-//     "user_id": 127419,
-//     "views": 127450,
-//     "webformatHeight": 472,
-//     "webformatURL": "https://pixabay.com/get/57e5d54b4c53af14f6da8c7dda793376173cd8e7524c704c702873dc9f44c551_640.jpg",
-//     "webformatWidth": 640
-//   }
-
-// Тебе интересны следующие свойства:
-
-// webformatURL - ссылка на маленькое изображение для списка карточек
-// largeImageURL - ссылка на большое изображение (смотри пункт 'дополнительно')
-// likes - количество лайков
-// views - количество просмотров
-// comments - количество комментариев
-// downloads - количество загрузок
+import { error, alert } from "../node_modules/@pnotify/core/dist/PNotify.js";
+import "../node_modules/@pnotify/core/dist/PNotify.css";
+import "../node_modules/@pnotify/mobile/dist/PNotifyMobile.css";
+import "../node_modules/@pnotify/core/dist/BrightTheme.css";
 
 const key = "18943342-b8026c260dd173166ae4011d3";
-const apiUrl = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=cat&page=1&per_page=12&key=${key}`;
-const inputRef = document.querySelector("form > input");
-console.dir(inputRef);
 
-fetch(apiUrl)
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+export default {
+  searchQuery: "",
+  page: 1,
+
+  fetchImages() {
+    const apiUrl = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${key}`;
+
+    return fetch(apiUrl)
+      .then((res) => res.json())
+      .then(({ hits }) => {
+        if (hits.length === 0) {
+          alert({
+            delay: 400,
+            text: "No images for your query! :(",
+          });
+        } else {
+          this.page += 1;
+          return hits;
+        }
+      })
+      .catch((error) =>
+        error({
+          delay: 400,
+          text: "Oops, something went wrong!",
+        }),
+      );
+  },
+
+  resetPage() {
+    this.page = 1;
+  },
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  },
+};
--- src/index.js
+++ src/index.js
@@ -1,2 +1,70 @@
 import css from "./css/style.css";
-import "./apiService.js";
+import "../node_modules/basiclightbox/dist/basicLightbox.min.css";
+import imageTemplate from "./imageTemplate.hbs";
+import apiService from "./apiService.js";
+var debounce = require("lodash.debounce");
+const basicLightbox = require("basiclightbox");
+
+const inputRef = document.querySelector("form > input");
+const gallery = document.querySelector(".gallery");
+const spinnerRef = document.querySelector(".spinner");
+
+const updateMarkup = function (markup) {
+  setTimeout(() => {
+    gallery.insertAdjacentHTML("beforeend", imageTemplate(markup));
+    spinnerRef.classList.add("is-hidden");
+    document.documentElement.scrollTop > 100
+      ? window.scrollTo({
+          top:
+            document.documentElement.scrollTop +
+            document.documentElement.clientHeight -
+            30,
+          behavior: "smooth",
+        })
+      : "";
+  }, 500);
+};
+
+const renderGallery = function (event) {
+  event.preventDefault();
+  gallery.innerHTML = "";
+  spinnerRef.classList.remove("is-hidden");
+
+  if (event.target.value !== "") {
+    apiService.query = event.target.value;
+
+    apiService.resetPage();
+    apiService.fetchImages().then((hits) => {
+      updateMarkup(hits);
+    });
+  }
+};
+
+inputRef.addEventListener("input", debounce(renderGallery, 500));
+
+window.addEventListener("scroll", () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+  if (clientHeight + scrollTop >= scrollHeight) {
+    apiService.fetchImages().then((hits) => updateMarkup(hits));
+    spinnerRef.classList.remove("is-hidden");
+  }
+});
+
+gallery.addEventListener("click", (event) => {
+  event.path.forEach((el) => {
+    if (el.nodeName === "LI") {
+      const instance = basicLightbox.create(`
+    <img src=${el.dataset.link}>
+`);
+      instance.show();
+    }
+  });
+});
+
+// loadMoreBtn.addEventListener("click", () => {
+//   apiService.fetchImages().then((hits) => updateMarkup(hits));
+//   window.scrollTo({
+//     top: document.documentElement.offsetHeight,
+//     behavior: "smooth",
+//   });
+// });
